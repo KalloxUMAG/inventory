04584745dcacf029d15911fa66948a31e58179cc
Kallox <d34d.mc@gmail.com>
2023-04-01 21:03:24 -0400

    Session get change to try - finally
    Resolve to much connections problem

--- Backend/config/database.py
+++ Backend/config/database.py
@@ -10,12 +10,18 @@ port = 5432
 
 DATABASE_URL = f"postgresql://{username}:{password}@{ip}:{port}/{database}"
 engine = create_engine(DATABASE_URL, pool_size=20, max_overflow=0)
-session_factory = sessionmaker(bind=engine)
-Session = scoped_session(session_factory)
+SessionLocal = sessionmaker(bind=engine, autoflush=False)
+#Session = scoped_session(session_factory)
 
 class Base(DeclarativeBase):
     pass
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
-def get_session(): 
-    return Session
+#def get_session(): 
+    #return Session
