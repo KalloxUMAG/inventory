3bc66a9b8d0200382d4a3647809186da8c2d2bc5
Kallox <d34d.mc@gmail.com>
2023-04-16 01:58:26 -0300

    Carousel image 100%

--- Frontend/src/components/Carousel.vue
+++ Frontend/src/components/Carousel.vue
@@ -1,7 +1,9 @@
 <template>
   <q-carousel v-model="slide" animated arrows thumbnails infinite :height="windowHeight" control-color="amber" class="rounded-borders">
-    <q-carousel-slide v-if="this.images == null" :name="1" img-src="https://cdn.quasar.dev/img/mountains.jpg" />
-    <q-carousel-slide v-for="image in images" :key="image.id" :name="image.id" :img-src="image.path"/>
+
+      <q-carousel-slide v-if="this.images == null" :name="1" img-src="https://cdn.quasar.dev/img/mountains.jpg" />
+      <q-carousel-slide v-for="image in images" :key="image.id" :name="image.id" :img-src="image.path"/>
+
   </q-carousel>
 </template>
 
@@ -44,5 +46,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+  .q-carousel__slide{
+    background-size: 100% 100%;
+    background-repeat: no-repeat;
+  }
 </style>
