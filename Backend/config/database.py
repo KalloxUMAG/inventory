1d3853fbf160859b12139cc2de9e205493b93c7c
KalloxUMAG <clramire@umag.cl>
2023-05-15 20:41:41 -0300

    .env file config

--- Backend/config/database.py
+++ Backend/config/database.py
@@ -1,17 +1,20 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import sessionmaker, scoped_session
+from dotenv import load_dotenv, dotenv_values
+import os
 
-username = "kallox"
-password = "Claudioxx124"
-database = "inventory"
-ip = "localhost"
-port = 5432
+config = dotenv_values(".env")
+
+username = config['USERNAME']
+password = config['PASSWORD']
+database = config['DATABASE']
+ip = config['IP']
+port = config['PORT']
 
 DATABASE_URL = f"postgresql://{username}:{password}@{ip}:{port}/{database}"
 engine = create_engine(DATABASE_URL, pool_size=20, max_overflow=0)
 SessionLocal = sessionmaker(bind=engine, autoflush=False)
-#Session = scoped_session(session_factory)
 
 class Base(DeclarativeBase):
     pass
