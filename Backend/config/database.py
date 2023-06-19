48db7b7ff1c15283ad77779874003b8a1089d845
Diego Alvarez S <dialvarezs@gmail.com>
2023-06-19 17:27:05 -0300

    feat: Podman deployment (WIP)

--- Backend/config/database.py
+++ Backend/config/database.py
@@ -1,22 +1,38 @@
+from os import environ
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import sessionmaker
 from dotenv import dotenv_values
 
-config = dotenv_values(".env")
+production = environ.get('PRODUCTION', False)
 
-username = config['USERNAME']
-password = config['PASSWORD']
-database = config['DATABASE']
-ip = config['IP']
-port = config['PORT']
+if production:
+    host = environ.get('DB_HOST')
+    port = environ.get('DB_PORT', 5432)
+    username = environ.get('DB_USER')
+    password = environ.get('DB_PASSWORD')
+    database = environ.get('DB_NAME')
 
-DATABASE_URL = f"postgresql://{username}:{password}@{ip}:{port}/{database}"
+    DATABASE_URL = f"postgresql://{username}:{password}@{host}:{port}/{database}"
+else:
+    config = dotenv_values(".env")
+
+    username = config['USERNAME']
+    password = config['PASSWORD']
+    database = config['DATABASE']
+    ip = config['IP']
+    port = config['PORT']
+
+    DATABASE_URL = f"postgresql://{username}:{password}@{ip}:{port}/{database}"
+
 engine = create_engine(DATABASE_URL, pool_size=20, max_overflow=0)
 SessionLocal = sessionmaker(bind=engine, autoflush=False)
 
+
 class Base(DeclarativeBase):
     pass
+
 
 def get_db():
     db = SessionLocal()
@@ -25,5 +41,5 @@ def get_db():
     finally:
         db.close()
 
-#def get_session(): 
-    #return Session
+# def get_session():
+# return Session
