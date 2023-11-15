5efc5ba7cba5c8efad3b5044b7546d71d27ff5e9
Sergio Sepulveda <sergio.sepulveda@acroventus.com>
2023-11-15 00:33:24 -0300

    feat: implementation actions when axios bridge

---
+++ Frontend/src/stores/user.js
@@ -0,0 +1,21 @@
+import { defineStore } from "pinia";
+import { api } from "src/boot/axios";
+
+export const useUserStore = defineStore("user", {
+  actions: {
+    async fetchUser({ fullname = null, email = null }) {
+      try {
+        const { data } = await api.get(`users/search_users`, {
+          params: {
+            fullname,
+            email,
+          },
+        });
+        return data;
+      } catch (error) {
+        console.log(error);
+        return [];
+      }
+    },
+  },
+});
