ec1951aa16729d84482b098980150c59b982769c
KalloxUMAG <clramire@umag.cl>
2024-04-26 16:21:26 -0300

    Feat[FormSection]: Component to create a title and white background on forms

---
+++ Frontend/src/components/Form/FormSection.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="form-section__title">
+        {{ title }}
+    </div>
+    <q-card class="my-card q-gutter-md col-12 q-pa-md gap-lg" flat bordered>
+        <slot></slot>
+    </q-card>
+</template>
+
+<script setup>
+    const props = defineProps({
+        title: String
+    })
+    const { title } = props
+</script>
+
+<style scoped>
+
+.form-section__title {
+    font-size: 1.5rem;
+    font-weight: 500;
+}
+
+.my-card {
+  width: 100%;
+  border-radius: 12px !important;
+}
+</style>
