20071a158cc1abc4fc02ccf76ab344cded2164ff
Claudio Ramirez <claudio.ramirez@umag.cl>
2024-05-22 12:28:50 -0300

    Fix[LoginLayout]: Removed unused styles

--- Frontend/src/layouts/LoginLayout.vue
+++ Frontend/src/layouts/LoginLayout.vue
@@ -7,20 +7,3 @@
     </q-page-container>
   </q-layout>
 </template>
-
-<script setup></script>
-
-<style lang="scss">
-.q-header {
-  background-color: #2196f3;
-}
-
-.q-drawer {
-  background-color: #ffffff;
-}
-
-.my-menu-link {
-  color: #fff;
-  background: rgba(0, 0, 0, 0.9);
-}
-</style>
