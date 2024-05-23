41415af9752d528ecd1ccd4c85a30410e45b84a5
Claudio Ramirez <claudio.ramirez@umag.cl>
2024-05-23 13:20:53 -0300

    Feat[StatisticsSection]: Counters by props

--- Frontend/src/components/statistics/StatisticsSection.vue
+++ Frontend/src/components/statistics/StatisticsSection.vue
@@ -1,17 +1,37 @@
 <template>
   <div class="statistics-container">
-    <StatisticCounter color="#23978a" icon="biotech" title="Equipos" value="1" />
-    <StatisticCounter color="#48b06d" icon="science" title="Insumos" value="0" />
-    <StatisticCounter color="#644d99" icon="settings" title="Mantenimientos próximos" value="1" />
-    <StatisticCounter color="#104e95" icon="call" title="Insumos críticos" value="0" />
+    <StatisticCounter color="#23978a" icon="biotech" title="Equipos" :value="equipments" />
+    <StatisticCounter color="#48b06d" icon="science" title="Insumos" :value="supplies" />
+    <StatisticCounter color="#644d99" icon="settings" title="Mantenimientos próximos" :value="criticalEquipments" />
+    <StatisticCounter color="#104e95" icon="call" title="Insumos críticos" :value="criticalSupplies" />
   </div>
 </template>
 
 <script setup>
+import { toRefs } from 'vue'
 import StatisticCounter from 'src/components/statistics/StatisticsCounter.vue'
-import { getEquipmentList } from '/src/services'
 
-const equipments = getEquipmentList()
+const props = defineProps({
+  equipments: {
+    type: Number,
+    default: 0,
+  },
+  supplies: {
+    type: Number,
+    default: 0,
+  },
+  criticalEquipments: {
+    type: Number,
+    default: 0,
+  },
+  criticalSupplies: {
+    type: Number,
+    default: 0,
+  },
+})
+
+const { equipments, supplies, criticalEquipments, criticalSupplies } = toRefs(props)
+
 </script>
 
 <style scoped>
