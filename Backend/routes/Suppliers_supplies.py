13e11348058d0553180f3a30059a3a1da6a414bf
Claudio Ramirez <claudio.ramirez@umag.cl>
2024-07-04 16:01:33 -0300

    Feat[Suppliers supplies service]: service created

--- Backend/routes/Suppliers_supplies.py
+++ Backend/routes/Suppliers_supplies.py
@@ -5,7 +5,9 @@ from sqlalchemy.orm import Session
 from starlette.status import HTTP_201_CREATED, HTTP_404_NOT_FOUND
 
 from config.database import get_db
-from models.models import Supplier, SuppliersHasSupplies, Supply
+from services.suppliers_supplies import SupplierSupplyService
+from services.suppliers import SupplierService
+from services.supplies import SupplyService
 from schemas.supplier_supply_schema import GetSupplierSupplySchema, SupplierSupplySchema
 
 from auth.auth_bearer import JWTBearer
@@ -15,50 +17,32 @@ suppliers_supplies = APIRouter(
     tags=["suppliers"],
     prefix="/api/suppliers_supplies",
 )
+service = SupplierSupplyService()
+supplier_service = SupplierService()
+supply_service = SupplyService()
 
 
 @suppliers_supplies.get("", response_model=List[SupplierSupplySchema])
-def get_suppliers_supplies(db: Session = Depends(get_db)):
-    result = db.query(SuppliersHasSupplies).all()
+async def get_suppliers_supplies(db: Session = Depends(get_db)):
+    result = await service.get_suppliers_supplies(db)
     return result
 
 
 @suppliers_supplies.post("", status_code=HTTP_201_CREATED)
-def add_supplier_supply(
+async def add_supplier_supply(
     supplier_supply: SupplierSupplySchema, db: Session = Depends(get_db)
 ):
-    db_supplier = (
-        db.query(Supplier).filter(Supplier.id == supplier_supply.supplier_id).first()
-    )
+    db_supplier = await supplier_service.get_supplier(supplier_supply.supplier_id, db)
     if not db_supplier:
         return Response(status_code=HTTP_404_NOT_FOUND)
-    db_supply = db.query(Supply).filter(Supply.id == supplier_supply.supply_id).first()
+    db_supply = await supply_service.get_supply(supplier_supply.supply_id, db)
     if not db_supply:
         return Response(status_code=HTTP_404_NOT_FOUND)
-    new_supplier_supply = SuppliersHasSupplies(
-        supplier_id=supplier_supply.supplier_id,
-        supply_id=supplier_supply.supply_id,
-        cost=supplier_supply.cost,
-    )
-    db.add(new_supplier_supply)
-    db.commit()
-    db.refresh(new_supplier_supply)
+    new_supplier_supply = await service.add_supplier_supply(supplier_supply, db)
     return Response(status_code=HTTP_201_CREATED)
 
 
 @suppliers_supplies.get("/{supply_id}", response_model=List[GetSupplierSupplySchema])
-def get_suppliers_supply(supply_id: int, db: Session = Depends(get_db)):
-    result = (
-        db.query(
-            SuppliersHasSupplies.supplier_id,
-            SuppliersHasSupplies.supply_id,
-            SuppliersHasSupplies.cost,
-            Supplier.name.label("name"),
-            Supplier.rut.label("rut"),
-            Supplier.city_address.label("city_address"),
-        )
-        .filter(SuppliersHasSupplies.supply_id == supply_id)
-        .outerjoin(Supplier, Supplier.id == SuppliersHasSupplies.supplier_id)
-        .all()
-    )
-    return result
+async def get_suppliers_supply(supply_id: int, db: Session = Depends(get_db)):
+    suppliers = await service.get_suppliers_by_supply(supply_id, db)
+    return suppliers
---
+++ Backend/services/suppliers_supplies.py
@@ -0,0 +1,31 @@
+from sqlalchemy.orm import Session
+from models.models import SuppliersHasSupplies, Supplier
+
+class SupplierSupplyService:
+    async def get_suppliers_supplies(self, db: Session):
+        return db.query(SuppliersHasSupplies).all()
+    async def get_suppliers_by_supply(self, supply_id: int, db: Session):
+        result = (
+            db.query(
+                SuppliersHasSupplies.supplier_id,
+                SuppliersHasSupplies.supply_id,
+                SuppliersHasSupplies.cost,
+                Supplier.name.label("name"),
+                Supplier.rut.label("rut"),
+                Supplier.city_address.label("city_address"),
+            )
+            .filter(SuppliersHasSupplies.supply_id == supply_id)
+            .outerjoin(Supplier, Supplier.id == SuppliersHasSupplies.supplier_id)
+            .all()
+        )
+        return result
+    async def add_supplier_supply(self, supplier_supply: SuppliersHasSupplies, db: Session):
+        new_supplier_supply = SuppliersHasSupplies(
+            supplier_id=supplier_supply.supplier_id,
+            supply_id=supplier_supply.supply_id,
+            cost=supplier_supply.cost,
+        )
+        db.add(new_supplier_supply)
+        db.commit()
+        db.refresh(new_supplier_supply)
+        return new_supplier_supply
