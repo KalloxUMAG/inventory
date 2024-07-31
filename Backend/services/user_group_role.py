eca16dd43a378eb9d7afd3e45e6dcb1a967b768a
Claudio Ramirez <claudio.ramirez@umag.cl>
2024-07-31 17:19:04 -0300

    Feat[User Role]: Get user role in group

--- Backend/services/user_group_role.py
+++ Backend/services/user_group_role.py
@@ -10,6 +10,9 @@ class UserGroupRoleService:
     async def get_user_groups_and_roles(self, user_id: int, db: Session):
         result = db.query(Users.id.label("user_id"), Users.fullname.label("user_name"), Groups.id.label("group_id"), Groups.name.label("group_name"), Role.id.label("role_id"), Role.name.label("role_name"), Role.create, Role.read, Role.update, Role.delete).filter(Users.id == user_id).outerjoin(UserGroupRoleRelation, UserGroupRoleRelation.user_id == Users.id).outerjoin(Groups, Groups.id == UserGroupRoleRelation.group_id).outerjoin(Role, Role.id == UserGroupRoleRelation.role_id).all()
         return result
+    async def get_user_group_roles(self, user_id: int, group_id: int, db: Session):
+        result = db.query(UserGroupRoleRelation.user_id, Role.id.label("role_id"), Role.name.label("role_name"), Role.create, Role.read, Role.update, Role.delete).outerjoin(Role, Role.id == UserGroupRoleRelation.role_id).filter(UserGroupRoleRelation.user_id == user_id, UserGroupRoleRelation.group_id == group_id).first()
+        return result
     async def add_user_group_role(self, user_group_role: UserGroupRoleSchema, db: Session):
         relation = UserGroupRoleRelation(user_id=user_group_role.user_id, group_id=user_group_role.group_id, role_id=user_group_role.role_id)
         db.add(relation)
