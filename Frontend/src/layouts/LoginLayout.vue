1da0f039597606a23d9545e83d18183caa881307
KalloxUMAG <clramire@umag.cl>
2024-08-19 13:22:51 -0300

    Feat[Login]: More responsive login page

--- Frontend/src/layouts/LoginLayout.vue
+++ Frontend/src/layouts/LoginLayout.vue
@@ -1,9 +1,17 @@
 <template>
   <q-layout view="hHh LpR fFf">
     <q-page-container>
-      <q-page class="flex flex-center bg-grey-2">
+      <q-page class="flex bg-image flex-center">
         <router-view />
       </q-page>
     </q-page-container>
   </q-layout>
 </template>
+
+<style scoped>
+.bg-image {
+  background-image: url('/images/cadi_building.jpg');
+  background-size: cover;
+  background-position: center;
+}
+</style>
--- Frontend/src/pages/Login.vue
+++ Frontend/src/pages/Login.vue
@@ -1,50 +1,44 @@
 <template>
-  <q-form ref="loginForm" @submit.prevent="onSubmit">
-    <q-card class="q-pa-md shadow-2 my_card" bordered>
-      <q-card-section class="text-center">
-        <div class="text-h4 text-weight-bold">
-          Sistema de Inventario CADI UMAG
-        </div>
-      </q-card-section>
-      <q-card-section class="text-center">
-        <div class="text-grey-9 text-h5 text-weight-bold">
-          Iniciar sesion
-        </div>
-      </q-card-section>
-      <q-card-section>
-        <q-input v-model="email" dense outlined label="Correo">
-          <template #prepend>
-            <q-icon name="mail" />
-          </template>
-        </q-input>
-        <q-input
-          v-model="password"
-          dense
-          outlined
-          icon
-          class="q-mt-md"
-          type="password"
-          label="Contrasena"
-        >
-          <template #prepend>
-            <q-icon name="key" />
-          </template>
-        </q-input>
-      </q-card-section>
-      <q-card-section>
-        <q-btn
-          style="border-radius: 8px"
-          color="dark"
-          rounded
-          size="md"
-          label="Iniciar sesion"
-          no-caps
-          class="full-width"
-          type="submit"
-        />
-      </q-card-section>
-    </q-card>
-  </q-form>
+  <q-card class="no-shadow">
+    <q-card-section class="text-center">
+      <div class="text-h4 text-weight-bold">
+        Sistema de Inventario CADI UMAG
+      </div>
+    </q-card-section>
+    <q-card-section class="q-mx-none">
+      <q-form ref="loginForm" @submit.prevent="onSubmit">
+        <div class="text-grey-8">Correo electrónico</div>
+        <q-input v-model="email" dense outlined label="Correo electrónico">
+            <template #prepend>
+              <q-icon name="mail" />
+            </template>
+          </q-input>
+          <div class="text-grey-8 q-mt-md">Contraseña</div>
+          <q-input
+            v-model="password"
+            dense
+            outlined
+            icon
+            type="password"
+            label="Contraseña"
+          >
+            <template #prepend>
+              <q-icon name="key" />
+            </template>
+          </q-input>
+          <q-btn
+            style="border-radius: 8px"
+            color="dark"
+            rounded
+            size="md"
+            label="Iniciar sesion"
+            no-caps
+            class="full-width q-mt-lg"
+            type="submit"
+          />
+      </q-form>
+    </q-card-section>
+  </q-card>
   <q-inner-loading
     :showing="loading"
     label="Iniciando sesion..."
