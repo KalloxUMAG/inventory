1cea4a3104a1f4c37fd906f1c173d0d63377e93d
KalloxUMAG <clramire@umag.cl>
2024-08-26 22:02:53 -0300

    Fix[Statistics]: Height set to 100% and removed unused css

--- Frontend/src/components/statistics/StatisticsCounter.vue
+++ Frontend/src/components/statistics/StatisticsCounter.vue
@@ -40,44 +40,7 @@ const style = computed(() => {
 </script>
 
 <style scoped>
-.statistic-container {
-  align-items: center;
-  box-shadow: 0 1px 5px rgba(0, 0, 0, 0.2), 0 2px 2px rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.12);
-  display: flex;
-  height: 90px;
-  justify-content: center;
-  overflow: hidden;
-  width: 200px;
-  min-width: 200px;
-}
-
-.statistic-icon {
-  align-items: center;
-  background-color: #e54d21;
-  display: flex;
-  height: 90px;
-  justify-content: center;
-  width: 90px;
-}
-
-.statistic-info {
-  align-items: center;
-  background-color: white;
-  display: flex;
-  flex-direction: column;
-  height: 90px;
-  justify-content: center;
-  width: 110px;
-  .statistic-value {
-    font-size: 20px;
-    font-weight: bold;
-    margin: 0px;
-    text-align: center;
-  }
-  .statistic-title {
-    font-size: 12px;
-    margin: 0px;
-    text-align: center;
-  }
+.q-item {
+  height: 100%;
 }
 </style>
