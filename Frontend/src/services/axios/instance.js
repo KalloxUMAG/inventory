875e9e32be808685c43e46d1456ecaebc5abf66f
KalloxUMAG <clramire@umag.cl>
2024-09-20 19:23:49 -0300

    Fix[axios service]: Old token not longer remain in request

--- Frontend/src/services/axios/instance.js
+++ Frontend/src/services/axios/instance.js
@@ -1,19 +1,26 @@
 import { Cookies } from 'quasar'
 import axios from 'axios'
 
-export const reqInstance = axios.create({
-  headers: {
-    Authorization: `Bearer ${Cookies.get('CATGInventoryToken')}`,
-  },
+export const reqInstance = axios.create()
+
+// Interceptor para agregar el token en cada solicitud
+reqInstance.interceptors.request.use((config) => {
+  const token = Cookies.get('CATGInventoryToken') // Obtén el token más reciente
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}` // Actualiza el header Authorization con el token
+  }
+  return config
+}, (error) => {
+  return Promise.reject(error)
 })
 
 export async function sendRequest(config) {
+  console.log(Cookies.get('CATGInventoryToken')) // Verifica que el token sea el correcto
   try {
     const response = await reqInstance(config)
     return response
-  }
-  catch (error) {
-    if (error.response.status === 403) {
+  } catch (error) {
+    if (error.response && error.response.status === 403) {
       Cookies.remove('CATGInventoryToken')
       Cookies.remove('CATGInventoryFullname')
     }
